8ff0b661710432c16afd6978d6a6fc6543dd2678
iori <iori.nishizawa@gmail.com>
2023-04-10 17:02:55 +0900

    fef: Refactor Database class to use Parser for SQL statement parsing.

--- lib/database.rb
+++ lib/database.rb
@@ -3,7 +3,23 @@ class Database
 
   def initialize
     @tables = {}
+    @parser = Parser.new
   end
+
+  def execute(sql)
+    case sql.strip
+    when /^CREATE TABLE/
+      create_table(*@parser.parse_create_table(sql))
+    when /^INSERT INTO/
+      insert(*@parser.parse_insert(sql))
+    when /^SELECT/
+      select(*@parser.parse_select(sql))
+    else
+      raise "Unsupported SQL statement: #{sql}"
+    end
+  end
+
+  private
 
   def create_table(name, columns)
     @tables[name] = Table.new(name, columns)
@@ -15,44 +31,5 @@ class Database
 
   def select(name, conditions = {})
     @tables[name].select(conditions)
-  end
-
-  def execute(sql)
-    case sql.strip
-    when /^CREATE TABLE/
-      parse_create_table(sql)
-    when /^INSERT INTO/
-      parse_insert(sql)
-    when /^SELECT/
-      parse_select(sql)
-    else
-      raise "Unsupported SQL statement: #{sql}"
-    end
-  end
-
-  private
-
-  def parse_create_table(sql)
-    table_name = sql[/^CREATE TABLE (\w+)/, 1]
-    columns = sql[/\((.+)\)/, 1].split(',').map(&:strip)
-    create_table(table_name, columns)
-  end
-
-  def parse_insert(sql)
-    table_name = sql[/^INSERT INTO (\w+)/, 1]
-    values = sql[/VALUES \((.+)\)/, 1].split(',').map do |value|
-      value.strip.gsub(/'(.+)'/, '\1') # Remove single quotes
-    end
-    insert(table_name, values)
-  end
-
-  def parse_select(sql)
-    table_name = sql[/^SELECT .+ FROM (\w+)/, 1]
-    conditions_str = sql[/WHERE (.+)$/, 1]
-    conditions = conditions_str&.split('AND')&.map do |condition|
-      column, value = condition.strip.split('=').map(&:strip)
-      [column, value.gsub(/'(.+)'/, '\1')] # Remove single quotes
-    end&.to_h || {}
-    select(table_name, conditions)
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,2 +1,3 @@
 require_relative 'database'
 require_relative 'table'
+require_relative 'parser'
